fb21f8b6255c8c46234aeed642561df4cf800588
brianliu1412 <briangliu1@gmail.com>
2023-05-04 12:16:08 -0700

    Finished both JS files

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,59 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+let voices = [];
+
 function init() {
-  // TODO
+  let text = document.querySelector('textarea');
+  let image = document.querySelector('img');
+  let button = document.querySelector('button');
+  let voiceselect = document.querySelector('#voice-select');
+
+
+  
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+  
+    voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  
+
+  button.onclick = function(){
+    const utterThis = new SpeechSynthesisUtterance(text.value);
+    const selectedOption = voiceselect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        console.log(voices[i]);
+      }
+    }
+    
+    speechSynthesis.speak(utterThis);
+    image.src = "assets/images/smiling-open.png";
+    
+    utterThis.onend = (event) => {
+      image.src="assets/images/smiling.png";
+    }
+   
+  }
+  
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,72 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti()
+  const airhorn = document.querySelector("[value='air-horn']");
+  const partyhorn = document.querySelector("[value=party-horn");
+  const carhorn = document.querySelector("[value=car-horn");
+  let volume = document.getElementById("volume");
+  let images = document.getElementsByTagName("img");
+  const playsound = document.querySelector("button");
+  const hornSelector = document.getElementById("horn-select");
+  let audio = document.querySelector("audio")
+  
+  /* *
+   * This event is for changing the image at the top when a new value is selected in the dropdown menu.
+   * The associated mp3 file is also updated.
+   * */
+  hornSelector.addEventListener("change", updateImage);
+  function updateImage() {
+    let selected = hornSelector.value;
+    if(selected == "air-horn"){
+      images[0].src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+    } else if(selected == "car-horn"){
+      images[0].src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+    } else {
+      images[0].src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+    }
+  }
+  
+  /**
+  * This function updates the volume icon as someone moves the slider 
+  * depending on how loud they want the horn volume to be.
+  */
+  volume.oninput = function() {
+    audio.volume = volume.value / 100;
+    if (volume.value == 0) {
+      images[1].src = "assets/icons/volume-level-0.svg";
+    }
+    else if (volume.value > 1 && volume.value < 33) {
+      images[1].src = "assets/icons/volume-level-1.svg";
+    }
+    else if (volume.value > 34 && volume.value < 67) {
+      images[1].src = "assets/icons/volume-level-2.svg";
+    }
+    else {
+      images[1].src = "assets/icons/volume-level-3.svg";
+    }
+  }
+
+  /**
+   * This function is for when someone clicks the button to play the sound, the audio is played 
+   * and the confetti visual is shown on the website.
+   */
+  playsound.onclick = function() {
+    if (hornSelector.value != "select") {
+      audio.play();
+      if (hornSelector.value == "party-horn") {
+        jsConfetti.addConfetti( {
+          emojis: ['🎉', '🎊', '🫦'],
+          emojiSize: 70,
+          confettiNumber: 100,
+        })
+    }
+      
+    }
+    
+  }
 }
